38534c62f6fb4fd178a8b94be42a4d3eaaef1780
AnKiWoong <ankiwoong@gmail.com>
2020-03-09 20:01:33 +0900

    master

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="http://localhost\css\style.css" type="text/css" />
+    <title>Index</title>
+</head>
+
+<body id="target">
+    <header>
+        <h1>
+            <a href="http://localhost\index.php">JavaScript</a>
+        </h1>
+    </header>
+    <nav>
+        <ol>
+            <?php
+            echo file_get_contents("list.txt");
+            ?>
+        </ol>
+    </nav>
+
+    <div id="control">
+        <input type="button" value="white" id="white_btn" />
+        <input type="button" value="black" id="black_btn" />
+    </div>
+
+    <article>
+        <?php
+        if(empty($_GET['id']) == false){
+          echo file_get_contents($_GET['id'].".txt");
+        }SSSS
+        ?>
+    </article>
+
+    <script src="http://localhost/js/script.js"></script>
+</body>
+
+</html>
