bd845bd8a008f2f8e38ec410fa2eb4c475174f86
AnKiWoong <ankiwoong@gmail.com>
2020-03-22 13:52:02 +0900

    PHP Study

---
+++ php/number.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Number & Operatior</h1>
+    <h2>1 + 1</h2>
+    <?php
+    // echo 1;
+    echo 1+1;
+    // print(1);
+    ?>
+
+    <h2>2 - 1</h2>
+    <?php
+    echo 1*1;
+    ?>
+
+    <h2>2 * 1</h2>
+    <?php
+    echo 1*1;
+    ?>
+
+    <h2>2 / 1</h2>
+    <?php
+    echo 1*1;
+    ?>
+</body>
+
+</html>
---
+++ php/variable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Variable</h1>
+    <?php
+    $a = 1;
+    echo $a + 1;
+    ?>
+
+    <?php
+    $name = 'ankiwoong';
+    echo 'Lorem ipsum dolor, sit amet consectetur adipisicing'.$name.'elit.
+    Magnam provident nostrum'.$name.'quaerat, aspernatur illo assumenda nihil cum labore, 
+    numquam doloremque delectus ab! Labore, veritatis nostrum dignissimos ankiwoong beatae alias rerum id.';
+    ?>
+</body>
+
+</html>
