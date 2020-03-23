ef86f6bbaff049584399727df4b2989a72e2d0cd
ankiwoong <ankiwoong@gmail.com>
2020-03-23 12:31:17 +0900

    PHP Study

---
+++ php/function.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Function</h1>
+    <?php
+        $str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dignissimos sit quos blanditiis sapiente est odio distinctio corporis obcaecati rem accusantium maiores impedit. Veniam in officiis eaque necessitatibus earum nostrum!";
+        echo $str;
+    ?>
+    <h2>strlen()</h2>
+    <?php
+        echo strlen($str);
+    ?>
+</body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+            echo $_GET['id'];
+        ?>
+    </h2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas voluptate voluptatem unde veritatis mollitia cumque doloremque perspiciatis harum ratione explicabo, asperiores a? Quae nam nostrum consequatur, quis sapiente harum illo.
+</body>
+</html>
