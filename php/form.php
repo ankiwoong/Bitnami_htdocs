1f660f1bbaa3932079eeaa61ade4486400b63827
ankiwoong <ankiwoong@gmail.com>
2020-03-26 13:02:21 +0900

    PHP Study

--- php/form.php
+++ php/form.php
@@ -1,3 +1,3 @@
 <?php
-file_put_contents('C:\Bitnami\wampstack-7.3.15-2\apache2\htdocs\etc'.$_POST['title'], $_POST['description']);
+    file_put_contents('../etc/'.$_POST['title'], $_POST['description']);
 ?>
