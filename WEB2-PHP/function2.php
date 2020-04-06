9b8dd7fee13cd91baf3e321038efa0dfad26ce53
ankiwoong <ankiwoong@gmail.com>
2020-04-06 12:05:14 +0900

    WEB2-PHP Study Code

---
+++ WEB2-PHP/function2.php
@@ -0,0 +1,7 @@
+function basic(){
+    print("Lorem ipsum dolor1<br>");
+    print("Lorem ipsum dolor2<br>");
+}
+basic();
+basic();
+basic();
---
+++ WEB2-PHP/function3.php
@@ -0,0 +1,6 @@
+function sum($left, $right){
+  print($left+$right);
+  print("<br>");
+}
+sum(2,4);
+sum(4,6);
