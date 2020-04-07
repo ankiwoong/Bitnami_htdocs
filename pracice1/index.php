3ec63c5fbd7aa7b9b9251f9ae6b8d05bf3980073
ankiwoong <ankiwoong@gmail.com>
2020-04-07 12:32:27 +0900

    WEB Application Class Summary

--- pracice1/index.php
+++ pracice1/index.php
@@ -10,7 +10,7 @@ UPDATE
 <?php
     $conn = mysqli_connect('localhost:3306', 'root', '1q2w3e4r5t');
     mysqli_select_db($conn, 'opentutorials2');
-    $sql = "SELECT topic.id, topic.title, user.name, topic.created FROM `topic` LEFT JOIN user ON topic.author = user.id WHERE id=".$id;
+    $sql = "SELECT * FROM `topic`";
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_assoc($result)){
         echo '<a href="index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a><br />';
@@ -26,4 +26,5 @@ UPDATE
     echo $row['created'];
     echo '<br />';
     echo $row['author'];
+    echo '<br />';
 ?>
