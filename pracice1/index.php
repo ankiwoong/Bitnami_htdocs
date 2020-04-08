eb8200b1a023f01c0d7b89a548bf92dc0ba7b756
ankiwoong <ankiwoong@gmail.com>
2020-04-09 08:35:40 +0900

    WEB Application Class Summary

--- pracice1/index.php
+++ pracice1/index.php
@@ -13,11 +13,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WEB Application Class Summary</title>
-    <style>
+    <style type="text/css">
     body {
         margin: 0;
     }
 
+    /* body class 값이 black 인 경우 배경 검정 / 글씨 흰색으로 바꿈*/
     body.black {
         background-color: black;
         color: white;
@@ -77,18 +78,23 @@
             $id = $_GET['id'];
             $sql = "SELECT * FROM topic WHERE id=".$id;
             $sql = "SELECT topic.id, topic.title, topic.description, user.name, topic.created FROM `topic` LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$id;
+            // mysqli_query : mysqli_connect 를 통해 연결된 객체를 이용하여 MySQL 쿼리를 실행
             $result = mysqli_query($conn, $sql);
+            // mysqli_fetch_assoc : 필드명(열이름, 키값)을 통해 데이터를 호출
             $row = mysqli_fetch_assoc($result);
         ?>
             <!-- 함축적인 표현이 있음 -->
+            <!-- 제목 -->
             <h2><?=htmlspecialchars($row['title'])?></h2>
-            <!-- | 기호를 사용하여 연결 할 수 있음 -->
+            <!-- | 기호를 사용하여 작성일자 | 작성자 표현 -->
             <div><?=htmlspecialchars($row['created'])?> | <?=htmlspecialchars($row['name'])?></div>
+            <!-- 내용 -->
             <div><?=htmlspecialchars($row['description'])?></div>
         </article>
-        <!-- 자바스크립트 구현 버튼 -->
-        <input type="button" name="name" value="White" onclick="document.getElementByid('body').className=''">
-        <input type="button" name="name" value="Black" onclick="document.getElementByid('body').className='black'">
+        <!-- 자바스크립트 구현 부분-->
+        <!-- 버튼을 클릭하면 색상 변하는 코드 구현 -->
+        <input type="button" name="name" value="White" onclick="document.getElementById('body').className='white'">
+        <input type="button" name="name" value="Black" onclick="document.getElementById('body').className='black'">
     </div>
 </body>
 
